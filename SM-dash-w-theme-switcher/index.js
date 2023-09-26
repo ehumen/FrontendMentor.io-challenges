c1403c6522f68bf9744876831ca7634fcef51be0
Yevhen Humen <105159428+ehumen@users.noreply.github.com>
2023-09-26 18:30:22 +0300

    Update index.js

--- SM-dash-w-theme-switcher/index.js
+++ SM-dash-w-theme-switcher/index.js
@@ -1,1 +1,45 @@
+const lightStyles = document.querySelectorAll('link[rel=stylesheet][href*=light]');
+const darkStyles = document.querySelectorAll('link[rel=stylesheet][href*=dark]');
+const switcherRadios = document.querySelectorAll('input[type=radio]');
+
+function getSavedScheme() {
+    return localStorage.getItem('color-scheme');
+}
+
+function setScheme(scheme) {
+    setMedia(scheme);
+    localStorage.setItem('color-scheme', scheme);
+}
+
+function setMedia(scheme) {
+    let lightMedia;
+    let darkMedia;
+
+    lightMedia = (scheme === 'light') ? 'all' : 'not all';
+    darkMedia = (scheme === 'dark') ? 'all' : 'not all';
+
+    [...lightStyles].forEach((link) => link.setAttribute('media', lightMedia));
+    [...darkStyles].forEach((link) => link.setAttribute('media', darkMedia));
+}
+
+function setupSwitcher() {
+    const savedScheme = getSavedScheme();
+
+    if (savedScheme !== null) {
+        const currentRadio = document.querySelector(`input[type=radio][id=${savedScheme}]`);
+        currentRadio.setAttribute('checked', 'true');
+    }
+    [...switcherRadios].forEach((radio) => {
+        radio.addEventListener('change', (event) => {
+            setScheme(event.target.id);
+        })
+    })
+}
+
+document.querySelector('.switcher').addEventListener('click',()=>{
+
+})
+
+setupSwitcher();
+setMedia(getSavedScheme());
 
